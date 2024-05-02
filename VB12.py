9b3b377828b3b41e26dd2e7d3b903383d1db7a92
ikeda042 <ikeda042main@gmail.com>
2024-05-02 11:32:55 +0900

    OD

--- VB12.py
+++ VB12.py
@@ -1,31 +1,40 @@
 # OD360 = 0.021C_V.B.12 + 0.003
 # V.B.12 = (OD360 - 0.003) / 0.021
-PROPIONIC_ACID = [0, 0.05, 0.10, 0.15, 0.20, 0.30]
+# PROPIONIC_ACID = [0, 0.05, 0.10, 0.15, 0.20, 0.30]
+
+
+def OD_to_conc(OD360):
+    return (OD360 - 0.003) / 0.021
+
 
 OD360_propionic_acid_p_1 = [0.444, 0.175, 0.162, 0.113, 0.135, 0.089]
 OD360_propionic_acid_n_1 = [0.269, 0.253, 0.156, 0.025, 0.129, 0.099]
 OD360_propionic_acid_p_2 = [0.329, 0.125, 0.126, 0.078, 0.098, 0.044]
 OD360_propionic_acid_n_2 = [0.244, 0.216, 0.119, 0.025, 0.079, 0.074]
 
-VB12_propionic_acid_p_1 = [
-    (OD360 - 0.003) / 0.021 for OD360 in OD360_propionic_acid_p_1
-]
+maxod = max(
+    max(OD360_propionic_acid_p_1),
+    max(OD360_propionic_acid_n_1),
+    max(OD360_propionic_acid_p_2),
+    max(OD360_propionic_acid_n_2),
+)
 
-VB12_propionic_acid_n_1 = [
-    (OD360 - 0.003) / 0.021 for OD360 in OD360_propionic_acid_n_1
-]
-
-VB12_propionic_acid_p_2 = [
-    (OD360 - 0.003) / 0.021 for OD360 in OD360_propionic_acid_p_2
-]
-
-VB12_propionic_acid_n_2 = [
-    (OD360 - 0.003) / 0.021 for OD360 in OD360_propionic_acid_n_2
-]
+VB12_propionic_acid_p_1 = [OD_to_conc(i) for i in OD360_propionic_acid_p_1]
+VB12_propionic_acid_n_1 = [OD_to_conc(i) for i in OD360_propionic_acid_n_1]
+VB12_propionic_acid_p_2 = [OD_to_conc(i) for i in OD360_propionic_acid_p_2]
+VB12_propionic_acid_n_2 = [OD_to_conc(i) for i in OD360_propionic_acid_n_2]
 
 import matplotlib.pyplot as plt
 
 fig = plt.figure(figsize=(8, 4))
+# STD line for VB12 OD360 = 0.021VB_conc + 0.003
+
+plt.plot(
+    [0, maxod],
+    [0.003, (maxod - 0.003) / 0.021],
+    color="red",
+)
+
 
 plt.scatter(OD360_propionic_acid_n_1, VB12_propionic_acid_n_1, color="black")
 plt.scatter(OD360_propionic_acid_p_1, VB12_propionic_acid_p_1, color="red")
@@ -33,3 +42,5 @@ plt.scatter(OD360_propionic_acid_n_2, VB12_propionic_acid_n_2, color="blue")
 plt.scatter(OD360_propionic_acid_p_2, VB12_propionic_acid_p_2, color="green")
 
 fig.savefig("images/VB121.png", dpi=500)
+
+print((0.44 - 0.003) / 0.021)
