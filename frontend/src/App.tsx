5df6d9d1dd34b563ad291339eeec16ae9bf3d23d
ikeda042 <ikeda042main@gmail.com>
2024-05-22 03:07:11 +0900

    T

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
 import TopPage from './components/TopPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <TopPage />
+      <Router>
+        <Routes>
+          <Route path="/" element={
+            <TopPage />
+          } />
+        </Routes>
+      </Router>
     </>
   );
 }
