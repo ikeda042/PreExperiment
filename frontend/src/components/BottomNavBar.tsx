f71e8ae86ab4d7e7b4e02f4dd46834001b24308d
ikeda042 <ikeda042main@gmail.com>
2024-05-22 03:51:12 +0900

    T

--- frontend/src/components/BottomNavBar.tsx
+++ frontend/src/components/BottomNavBar.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <Box sx={{ bgcolor: '#000', color: '#fff', p: 2, width: '100%', mt: 'auto' }}>
             <Typography variant="body1" align="center">
-                © 学生実験 2023
+                © 学生実験 2024
             </Typography>
         </Box>
     );
