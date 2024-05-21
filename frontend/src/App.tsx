4bd88a9e8b55080c180a0c7b81fe1c84746534b0
ikeda042 <ikeda042main@gmail.com>
2024-05-22 03:28:26 +0900

    APP

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import TopPage from './components/TopPage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import VB12 from './components/VB12';
+import Glucose
+  from './components/Glucose';
 function App() {
   return (
     <>
@@ -13,6 +15,9 @@ function App() {
           } />
           <Route path="/vb12" element={
             <VB12 />
+          } />
+          <Route path="/glucose" element={
+            <Glucose />
           } />
         </Routes>
       </Router>
