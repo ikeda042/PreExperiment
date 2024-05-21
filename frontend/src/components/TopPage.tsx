6b863bec12e12cd6bdaf239781f3247af53513b2
ikeda042 <ikeda042main@gmail.com>
2024-05-22 03:23:16 +0900

    T

--- frontend/src/components/TopPage.tsx
+++ frontend/src/components/TopPage.tsx
@@ -1,15 +1,57 @@
-import { Typography } from '@mui/material';
-import { Box } from '@mui/system';
+import { Box, Typography, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 import DrawerAppBar from './NavigationBar';
 import Footer from './BottomNavBar';
+import { Stack } from '@mui/material';
 
 export default function TopPage() {
     return (
-        <Box sx={{ bgcolor: "#f7f6f5", color: 'black' }}>
+        <Box sx={{ bgcolor: "#f7f6f5", color: 'black', display: 'flex', flexDirection: 'column', minHeight: '100vh', justifyContent: 'center', alignItems: 'center' }}>
             <DrawerAppBar />
-            <Typography variant="h1" align="center" sx={{ paddingTop: '20vh' }}>
-                テスト
-            </Typography>
+
+            <Stack spacing={2} direction="column" mt={20} mb={20}>
+                <Button
+                    variant="contained"
+                    component={Link}
+                    to="/vb12"
+                    sx={{
+                        bgcolor: 'black',
+                        color: 'white',
+                        padding: '16px 24px',
+                        fontSize: '18px'
+                    }}
+                >
+                    VB12の検量線を作成する
+                </Button>
+                <Button
+                    variant="contained"
+                    component={Link}
+                    to="/vb12"
+                    sx={{
+                        bgcolor: 'black',
+                        color: 'white',
+                        padding: '16px 24px',
+                        fontSize: '18px'
+                    }}
+                >
+                    VB12の検量線を作成する
+                </Button>
+                <Button
+                    variant="contained"
+                    component={Link}
+                    to="https://www.google.com"
+                    sx={{
+                        bgcolor: 'black',
+                        color: 'white',
+                        padding: '16px 24px',
+                        fontSize: '18px'
+                    }}
+                >
+                    VB12の検量線を作成する
+                </Button>
+            </Stack>
+
+
             <Footer />
         </Box>
     );
