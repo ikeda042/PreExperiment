92a9171be9f1b35b95746101b5594f00687fcfe5
ikeda042 <ikeda042main@gmail.com>
2024-05-22 02:25:40 +0900

    T

--- frontend/src/components/TopPage.tsx
+++ frontend/src/components/TopPage.tsx
@@ -1,9 +1,11 @@
 import { Typography } from '@mui/material';
 import { Box } from '@mui/system';
+import DrawerAppBar from './NavigationBar';
 
 export default function TopPage() {
     return (
         <Box sx={{ bgcolor: "#f7f6f5", color: 'black', minHeight: '100vh' }}>
+            <DrawerAppBar />
             <Typography variant="h1" align="center" sx={{ paddingTop: '20vh' }}>
                 テスト
             </Typography>
