bb519d298bddca8ee13a8f28d777ea9a541a210d
ikeda042 <ikeda042main@gmail.com>
2024-05-22 16:42:19 +0900

    GT

--- Glucose_01.py
+++ Glucose_01.py
@@ -160,5 +160,81 @@ plt.legend()
 # ticks to inside
 plt.tick_params(direction="in")
 plt.savefig("images/Glucose_01_HPLC.png", dpi=400)
+plt.close(fig)
 
 
+
+
+
+# なし 0
+PA_000_A1 = [0.051, 0.050, 0.046]
+PA_000_A2 = [0.363, 0.358, 0.247]
+PA_000_DELTA_OD340 = delta(PA_000_A1, PA_000_A2)
+
+PA_000_GLU_A1 = [L.predict_x(i) for i in PA_000_DELTA_OD340]
+
+# なし 0.05
+PA_005_A1 = [0.048, 0.051, 0.051]
+PA_005_A2 = [0.319, 0.055, 0.296]
+PA_005_DELTA_OD340 = delta(PA_005_A1, PA_005_A2)
+
+PA_005_GLU_A1 = [L.predict_x(i) for i in PA_005_DELTA_OD340]
+
+
+# なし 0.10
+PA_010_A1 = [-0.001, -0.015, 0.050]
+PA_010_A2 = [0.149, 0.229, 0.233]
+PA_010_DELTA_OD340 = delta(PA_010_A1, PA_010_A2)
+
+PA_010_GLU_A1 = [L.predict_x(i) for i in PA_010_DELTA_OD340]
+
+
+# なし 0.15
+PA_015_A1 = [0.050, 0.082, 0.049]
+PA_015_A2 = [0.487, 0.412, 0.236]
+PA_015_DELTA_OD340 = delta(PA_015_A1, PA_015_A2)
+
+PA_015_GLU_A1 = [L.predict_x(i) for i in PA_015_DELTA_OD340]
+
+# なし 0.20
+PA_020_A1 = [0.051, 0.051, 0.050]
+PA_020_A2 = [0.216, 0.258, 0.264]
+PA_020_DELTA_OD340 = delta(PA_020_A1, PA_020_A2)
+
+PA_020_GLU_A1 = [L.predict_x(i) for i in PA_020_DELTA_OD340]
+
+# なし 0.30
+PA_030_A1 = [0.055, 0.069, 0.051]
+PA_030_A2 = [0.456, 0.475, 0.271]
+PA_030_DELTA_OD340 = delta(PA_030_A1, PA_030_A2)
+
+PA_030_GLU_A1 = [L.predict_x(i) for i in PA_030_DELTA_OD340]
+
+# あり 0
+PA_000_A1_GLC = [0.048, 0.048, 0.048]
+PA_000_A2_GLC = [0.247, 0.314, 0.408]
+PA_000_DELTA_OD340_GLC = delta(PA_000_A1_GLC, PA_000_A2_GLC)
+
+PA_000_GLU_A1_GLC = [L.predict_x(i) for i in PA_000_DELTA_OD340_GLC]
+
+import matplotlib.pyplot as plt
+
+fig = plt.figure(figsize=(9, 6))
+
+days = [0, 1, 4]
+
+plt.plot(days, PA_000_DELTA_OD340, label="P.A. 0.00 M", marker="o")
+plt.plot(days, PA_005_DELTA_OD340, label="P.A. 0.05 M", marker="o")
+plt.plot(days, PA_010_DELTA_OD340, label="P.A. 0.10 M", marker="o")
+plt.plot(days, PA_015_DELTA_OD340, label="P.A. 0.15 M", marker="o")
+plt.plot(days, PA_020_DELTA_OD340, label="P.A. 0.20 M", marker="o")
+plt.plot(days, PA_030_DELTA_OD340, label="P.A. 0.30 M", marker="o")
+plt.plot(days, PA_000_DELTA_OD340_GLC, label="P.A. 0.00 M (GLC +)", marker="o")
+
+plt.xlabel("Day")
+plt.ylabel(r"$\Delta OD_{340}$")
+plt.legend()
+# ticks to inside
+plt.tick_params(direction="in")
+plt.savefig("images/Glucose_01.png", dpi=400)
+plt.close(fig)
