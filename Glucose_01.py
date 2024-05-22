76b27305b84e04ab8a264abff5f0bc6ed5872e33
ikeda042 <ikeda042main@gmail.com>
2024-05-22 16:40:27 +0900

    T

--- Glucose_01.py
+++ Glucose_01.py
@@ -155,7 +155,7 @@ plt.plot(days, PA_030_HPLC, label="P.A. 0.30 M", marker="o")
 plt.plot(days, PA_000_GLC_HPLC, label="P.A. 0.00 M (GLC +)", marker="o")
 
 plt.xlabel("Day")
-plt.ylabel("Glucose (mg/mL)")
+plt.ylabel("Glucose (mg/mL) -(HPLC)")
 plt.legend()
 # ticks to inside
 plt.tick_params(direction="in")
